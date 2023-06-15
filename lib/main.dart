d604559f9c1b5a089b00bdb00548a86ecc6a18a9
enaybi <89826525+enaybi@users.noreply.github.com>
2023-06-15 18:13:01 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:camera/camera.dart';
 import 'package:senyas/view/home.dart';
 import 'package:senyas/view/onboarding_screen.dart';
