4fc0f2b3085fafa70e7d0627068a26f1ba808bd3
enaybi <89826525+enaybi@users.noreply.github.com>
2023-06-15 17:45:33 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:camera/camera.dart';
-import 'package:senyas/view/chatpad.dart';
-import 'package:senyas/view/drawer.dart';
-import 'package:senyas/view/library_screen.dart';
+import 'package:senyas/view/home.dart';
 import 'package:senyas/view/onboarding_screen.dart';
-import 'package:senyas/view/look_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,127 +29,6 @@ class MyApp extends StatelessWidget {
         '/home': (context) => HomeScreen(camera: camera),
       },
       debugShowCheckedModeBanner: false,
-      // home: HomeScreen(camera: camera),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  final CameraDescription camera;
-
-  const HomeScreen({Key? key, required this.camera}) : super(key: key);
-
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  late CameraController _controller;
-  late Future<void> _initializeControllerFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = CameraController(
-      widget.camera,
-      ResolutionPreset.medium,
-    );
-
-    _initializeControllerFuture = _controller.initialize();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(backgroundColor: Colors.transparent, elevation: 0),
-      drawer: const AppDrawer(),
-      body: Column(
-        children: [
-          Expanded(
-            child: FutureBuilder<void>(
-              future: _initializeControllerFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  return CameraPreview(_controller);
-                } else {
-                  return const Center(child: CircularProgressIndicator());
-                }
-              },
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-          width: 250,
-          height: 60,
-          decoration: BoxDecoration(
-            color: Colors.grey,
-            borderRadius: BorderRadius.all(Radius.circular(30.0)),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => LookScreen()),
-                    );
-                  },
-                  backgroundColor: Colors.transparent,
-                  elevation: 0,
-                  child: SvgPicture.asset(
-                    'assets/library.svg',
-                    width: 45,
-                    height: 45,
-                  ),
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => LibraryScreen()),
-                    );
-                  },
-                  backgroundColor: Colors.transparent,
-                  elevation: 0,
-                  child: SvgPicture.asset(
-                    'assets/camera.svg',
-                    width: 45,
-                    height: 45,
-                  ),
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Chatpad()),
-                    );
-                  },
-                  backgroundColor: Colors.transparent,
-                  elevation: 0,
-                  child: SvgPicture.asset(
-                    'assets/chatpad.svg',
-                    width: 45,
-                    height: 45,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
