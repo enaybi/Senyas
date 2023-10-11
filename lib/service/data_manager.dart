b2031275b5e331b5e417e5b407b82b93bd29272e
TracyMarieAndrea <tracymarieandrea.madresta@wvsu.edu.ph>
2023-10-11 14:56:27 +0800

    added datamanager and model

---
+++ lib/service/data_manager.dart
@@ -0,0 +1,434 @@
+List<Map<String, dynamic>> dataList = [
+  {
+    'id': 0,
+    'imageCategory': 'Alphabet',
+    'imageName': 'A',
+    'imagePath': 'assets/classes_content/Alphabets/A.png'
+  },
+  {
+    'id': 1,
+    'imageCategory': 'Alphabet',
+    'imageName': 'B',
+    'imagePath': 'assets/classes_content/Alphabets/B.png'
+  },
+  {
+    'id': 2,
+    'imageCategory': 'Alphabet',
+    'imageName': 'C',
+    'imagePath': 'assets/classes_content/Alphabets/C.png'
+  },
+  {
+    'id': 3,
+    'imageCategory': 'Alphabet',
+    'imageName': 'D',
+    'imagePath': 'assets/classes_content/Alphabets/D.png'
+  },
+  {
+    'id': 4,
+    'imageCategory': 'Alphabet',
+    'imageName': 'E',
+    'imagePath': 'assets/classes_content/Alphabets/E.png'
+  },
+  {
+    'id': 5,
+    'imageCategory': 'Alphabet',
+    'imageName': 'F',
+    'imagePath': 'assets/classes_content/Alphabets/F.png'
+  },
+  {
+    'id': 6,
+    'imageCategory': 'Alphabet',
+    'imageName': 'G',
+    'imagePath': 'assets/classes_content/Alphabets/G.png'
+  },
+  {
+    'id': 7,
+    'imageCategory': 'Alphabet',
+    'imageName': 'H',
+    'imagePath': 'assets/classes_content/Alphabets/H.png'
+  },
+  {
+    'id': 8,
+    'imageCategory': 'Alphabet',
+    'imageName': 'I',
+    'imagePath': 'assets/classes_content/Alphabets/I.png'
+  },
+  {
+    'id': 9,
+    'imageCategory': 'Alphabet',
+    'imageName': 'J',
+    'imagePath': 'assets/classes_content/Alphabets/J.png'
+  },
+  {
+    'id': 10,
+    'imageCategory': 'Alphabet',
+    'imageName': 'K',
+    'imagePath': 'assets/classes_content/Alphabets/K.png'
+  },
+  {
+    'id': 11,
+    'imageCategory': 'Alphabet',
+    'imageName': 'L',
+    'imagePath': 'assets/classes_content/Alphabets/L.png'
+  },
+  {
+    'id': 12,
+    'imageCategory': 'Alphabet',
+    'imageName': 'M',
+    'imagePath': 'assets/classes_content/Alphabets/M.png'
+  },
+  {
+    'id': 13,
+    'imageCategory': 'Alphabet',
+    'imageName': 'N',
+    'imagePath': 'assets/classes_content/Alphabets/N.png'
+  },
+  {
+    'id': 14,
+    'imageCategory': 'Alphabet',
+    'imageName': 'O',
+    'imagePath': 'assets/classes_content/Alphabets/O.png'
+  },
+  {
+    'id': 15,
+    'imageCategory': 'Alphabet',
+    'imageName': 'P',
+    'imagePath': 'assets/classes_content/Alphabets/P.png'
+  },
+  {
+    'id': 16,
+    'imageCategory': 'Alphabet',
+    'imageName': 'Q',
+    'imagePath': 'assets/classes_content/Alphabets/Q.png'
+  },
+  {
+    'id': 17,
+    'imageCategory': 'Alphabet',
+    'imageName': 'R',
+    'imagePath': 'assets/classes_content/Alphabets/R.png'
+  },
+  {
+    'id': 18,
+    'imageCategory': 'Alphabet',
+    'imageName': 'S',
+    'imagePath': 'assets/classes_content/Alphabets/S.png'
+  },
+  {
+    'id': 19,
+    'imageCategory': 'Alphabet',
+    'imageName': 'T',
+    'imagePath': 'assets/classes_content/Alphabets/T.png'
+  },
+  {
+    'id': 20,
+    'imageCategory': 'Alphabet',
+    'imageName': 'U',
+    'imagePath': 'assets/classes_content/Alphabets/U.png'
+  },
+  {
+    'id': 21,
+    'imageCategory': 'Alphabet',
+    'imageName': 'V',
+    'imagePath': 'assets/classes_content/Alphabets/V.png'
+  },
+  {
+    'id': 22,
+    'imageCategory': 'Alphabet',
+    'imageName': 'W',
+    'imagePath': 'assets/classes_content/Alphabets/W.png'
+  },
+  {
+    'id': 23,
+    'imageCategory': 'Alphabet',
+    'imageName': 'X',
+    'imagePath': 'assets/classes_content/Alphabets/X.png'
+  },
+  {
+    'id': 24,
+    'imageCategory': 'Alphabet',
+    'imageName': 'Y',
+    'imagePath': 'assets/classes_content/Alphabets/Y.png'
+  },
+  {
+    'id': 25,
+    'imageCategory': 'Alphabet',
+    'imageName': 'Z',
+    'imagePath': 'assets/classes_content/Alphabets/Z.png'
+  },
+  {
+    'id': 26,
+    'imageCategory': 'Number',
+    'imageName': 'One',
+    'imagePath': 'assets/classes_content/Numbers/1.png'
+  },
+  {
+    'id': 27,
+    'imageCategory': 'Number',
+    'imageName': 'Two',
+    'imagePath': 'assets/classes_content/Numbers/2.png'
+  },
+  {
+    'id': 28,
+    'imageCategory': 'Number',
+    'imageName': 'Three',
+    'imagePath': 'assets/classes_content/Numbers/3.png'
+  },
+  {
+    'id': 29,
+    'imageCategory': 'Number',
+    'imageName': 'Four',
+    'imagePath': 'assets/classes_content/Numbers/4.png'
+  },
+  {
+    'id': 30,
+    'imageCategory': 'Number',
+    'imageName': 'Five',
+    'imagePath': 'assets/classes_content/Numbers/5.png'
+  },
+  {
+    'id': 31,
+    'imageCategory': 'Number',
+    'imageName': 'Six',
+    'imagePath': 'assets/classes_content/Numbers/6.png'
+  },
+  {
+    'id': 32,
+    'imageCategory': 'Number',
+    'imageName': 'Seven',
+    'imagePath': 'assets/classes_content/Numbers/7.png'
+  },
+  {
+    'id': 33,
+    'imageCategory': 'Number',
+    'imageName': 'Eight',
+    'imagePath': 'assets/classes_content/Numbers/8.png'
+  },
+  {
+    'id': 34,
+    'imageCategory': 'Number',
+    'imageName': 'Nine',
+    'imagePath': 'assets/classes_content/Numbers/9.png'
+  },
+  {
+    'id': 35,
+    'imageCategory': 'Number',
+    'imageName': 'Ten',
+    'imagePath': 'assets/classes_content/Numbers/10.png'
+  },
+  {
+    'id': 36,
+    'imageCategory': 'Basic Adjective',
+    'imageName': 'Bad',
+    'imagePath': 'assets/classes_content/BasicAdjective/Bad.jpg'
+  },
+  {
+    'id': 37,
+    'imageCategory': 'Basic Adjective',
+    'imageName': 'Difficult',
+    'imagePath': 'assets/classes_content/BasicAdjective/Difficult.jpg'
+  },
+  {
+    'id': 38,
+    'imageCategory': 'Basic Adjective',
+    'imageName': 'Easy',
+    'imagePath': 'assets/classes_content/BasicAdjective/Easy.png'
+  },
+  {
+    'id': 39,
+    'imageCategory': 'Basic Adjective',
+    'imageName': 'Fast',
+    'imagePath': 'assets/classes_content/BasicAdjective/Fast.png'
+  },
+  {
+    'id': 40,
+    'imageCategory': 'Basic Adjective',
+    'imageName': 'Good',
+    'imagePath': 'assets/classes_content/BasicAdjective/Good.png'
+  },
+  {
+    'id': 41,
+    'imageCategory': 'Basic Adjective',
+    'imageName': 'New',
+    'imagePath': 'assets/classes_content/BasicAdjective/New.jpg'
+  },
+  {
+    'id': 42,
+    'imageCategory': 'Basic Adjective',
+    'imageName': 'Old',
+    'imagePath': 'assets/classes_content/BasicAdjective/Old.png'
+  },
+  {
+    'id': 43,
+    'imageCategory': 'Basic Adjective',
+    'imageName': 'Slow',
+    'imagePath': 'assets/classes_content/BasicAdjective/Slow.png'
+  },
+  {
+    'id': 44,
+    'imageCategory': 'Basic Question',
+    'imageName': 'How',
+    'imagePath': 'assets/classes_content/BasicQuestions/How.jpg'
+  },
+  {
+    'id': 45,
+    'imageCategory': 'Basic Question',
+    'imageName': 'What',
+    'imagePath': 'assets/classes_content/BasicQuestions/What.jpg'
+  },
+  {
+    'id': 46,
+    'imageCategory': 'Basic Question',
+    'imageName': 'When',
+    'imagePath': 'assets/classes_content/BasicQuestions/When.png'
+  },
+  {
+    'id': 47,
+    'imageCategory': 'Basic Question',
+    'imageName': 'Where',
+    'imagePath': 'assets/classes_content/BasicQuestions/Where.png'
+  },
+  {
+    'id': 48,
+    'imageCategory': 'Basic Question',
+    'imageName': 'Who',
+    'imagePath': 'assets/classes_content/BasicQuestions/Who.png'
+  },
+  {
+    'id': 49,
+    'imageCategory': 'Basic Question',
+    'imageName': 'Why',
+    'imagePath': 'assets/classes_content/BasicQuestions/Why.png'
+  },
+  {
+    'id': 50,
+    'imageCategory': 'Family Tree',
+    'imageName': 'Baby',
+    'imagePath': 'assets/classes_content/FamilyTree/Baby.png'
+  },
+  {
+    'id': 51,
+    'imageCategory': 'Family Tree',
+    'imageName': 'Brother',
+    'imagePath': 'assets/classes_content/FamilyTree/Brother.png'
+  },
+  {
+    'id': 52,
+    'imageCategory': 'Family Tree',
+    'imageName': 'Father',
+    'imagePath': 'assets/classes_content/FamilyTree/Father.png'
+  },
+  {
+    'id': 53,
+    'imageCategory': 'Family Tree',
+    'imageName': 'Mother',
+    'imagePath': 'assets/classes_content/FamilyTree/Mother.png'
+  },
+  {
+    'id': 54,
+    'imageCategory': 'Family Tree',
+    'imageName': 'Sister',
+    'imagePath': 'assets/classes_content/FamilyTree/Sister.png'
+  },
+  {
+    'id': 55,
+    'imageCategory': 'Greeting',
+    'imageName': 'Good Afternoon',
+    'imagePath': 'assets/classes_content/Greetings/Good_Afternoon.jpg'
+  },
+  {
+    'id': 56,
+    'imageCategory': 'Greeting',
+    'imageName': 'Good Evening',
+    'imagePath': 'assets/classes_content/Greetings/Good_Evening.jpg'
+  },
+  {
+    'id': 57,
+    'imageCategory': 'Greeting',
+    'imageName': 'Good Morning',
+    'imagePath': 'assets/classes_content/Greetings/Good_Morning.png'
+  },
+  {
+    'id': 58,
+    'imageCategory': 'Greeting',
+    'imageName': 'Good Noon',
+    'imagePath': 'assets/classes_content/Greetings/Good_Noon.jpg'
+  },
+  {
+    'id': 59,
+    'imageCategory': 'Greeting',
+    'imageName': 'Hi',
+    'imagePath': 'assets/classes_content/Greetings/Hi.png'
+  },
+  {
+    'id': 60,
+    'imageCategory': 'Greeting',
+    'imageName': 'Please',
+    'imagePath': 'assets/classes_content/Greetings/Please.png'
+  },
+  {
+    'id': 61,
+    'imageCategory': 'Greeting',
+    'imageName': 'Sorry',
+    'imagePath': 'assets/classes_content/Greetings/Sorry.png'
+  },
+  {
+    'id': 62,
+    'imageCategory': 'Greeting',
+    'imageName': 'Thank you',
+    'imagePath': 'assets/classes_content/Greetings/Thank_you.png'
+  },
+  {
+    'id': 63,
+    'imageCategory': 'Greeting',
+    'imageName': 'Welcome',
+    'imagePath': 'assets/classes_content/Greetings/Welcome.png'
+  },
+  {
+    'id': 64,
+    'imageCategory': 'Pointing',
+    'imageName': 'Me',
+    'imagePath': 'assets/classes_content/Pointing/Me.jpg'
+  },
+  {
+    'id': 65,
+    'imageCategory': 'Pointing',
+    'imageName': 'My',
+    'imagePath': 'assets/classes_content/Pointing/My.png'
+  },
+  {
+    'id': 66,
+    'imageCategory': 'Pointing',
+    'imageName': 'They',
+    'imagePath': 'assets/classes_content/Pointing/They.jpg'
+  },
+  {
+    'id': 67,
+    'imageCategory': 'Pointing',
+    'imageName': 'This',
+    'imagePath': 'assets/classes_content/Pointing/This.png'
+  },
+  {
+    'id': 68,
+    'imageCategory': 'Pointing',
+    'imageName': 'Us',
+    'imagePath': 'assets/classes_content/Pointing/Us.jpg'
+  },
+  {
+    'id': 69,
+    'imageCategory': 'Pointing',
+    'imageName': 'We',
+    'imagePath': 'assets/classes_content/Pointing/We.jpg'
+  },
+  {
+    'id': 70,
+    'imageCategory': 'Pointing',
+    'imageName': 'You all',
+    'imagePath': 'assets/classes_content/Pointing/You_all.png'
+  },
+  {
+    'id': 71,
+    'imageCategory': 'Survival Word',
+    'imageName': 'Yes',
+    'imagePath': 'assets/classes_content/SurvivalWords/Yes.png'
+  },
+];
---
+++ lib/service/data_model.dart
@@ -0,0 +1,13 @@
+class FslClass {
+  final int id;
+  final String imageCategory;
+  final String imageName;
+  final String imagePath;
+
+  FslClass({
+    required this.id,
+    required this.imageCategory,
+    required this.imageName,
+    required this.imagePath,
+  });
+}
